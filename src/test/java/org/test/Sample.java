c75ba2ae47f32ae43a7eebb0b18b6fc0829e0195
ramesh0595 <rameshchinnu509@gmail.com>
2025-05-15 20:23:37 +0530

    Kumar code

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -7,6 +7,9 @@ public class Sample {
 	@Test
 	private void test() {
 		System.out.println("Test 1");
+		System.out.println("Test 4");
+		System.out.println("Test 5");
+		System.out.println("Test 6");
 	}
 
 }
